95e8e160da9e9bc4fc8746fea8c304495542cfbf
prince <princechaurasia21@gmail.com>
2023-01-06 16:58:32 +0530

    Added all Project related files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            this.dgId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgPhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgEmpId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.txt_EmpId = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(178, 29);
+            this.txt_Name.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(318, 30);
+            this.txt_Name.TabIndex = 1;
+            this.txt_Name.Leave += new System.EventHandler(this.txt_Name_Leave);
+            // 
+            // dgvEmployee
+            // 
+            this.dgvEmployee.AllowUserToDeleteRows = false;
+            this.dgvEmployee.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvEmployee.ColumnHeadersHeight = 34;
+            this.dgvEmployee.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgId,
+            this.dgName,
+            this.dgEmail,
+            this.dgPhone,
+            this.dgEmpId});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightCoral;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Yellow;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvEmployee.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvEmployee.GridColor = System.Drawing.SystemColors.MenuHighlight;
+            this.dgvEmployee.Location = new System.Drawing.Point(516, 30);
+            this.dgvEmployee.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvEmployee.Name = "dgvEmployee";
+            this.dgvEmployee.ReadOnly = true;
+            this.dgvEmployee.RowHeadersVisible = false;
+            this.dgvEmployee.RowHeadersWidth = 62;
+            this.dgvEmployee.RowTemplate.Height = 30;
+            this.dgvEmployee.Size = new System.Drawing.Size(604, 460);
+            this.dgvEmployee.TabIndex = 2;
+            this.dgvEmployee.DoubleClick += new System.EventHandler(this.dgvEmployee_DoubleClick);
+            // 
+            // dgId
+            // 
+            this.dgId.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.dgId.DataPropertyName = "Id";
+            this.dgId.HeaderText = "Id";
+            this.dgId.MinimumWidth = 8;
+            this.dgId.Name = "dgId";
+            this.dgId.ReadOnly = true;
+            this.dgId.Visible = false;
+            this.dgId.Width = 150;
+            // 
+            // dgName
+            // 
+            this.dgName.DataPropertyName = "Name";
+            this.dgName.HeaderText = "Name";
+            this.dgName.MinimumWidth = 8;
+            this.dgName.Name = "dgName";
+            this.dgName.ReadOnly = true;
+            this.dgName.Width = 150;
+            // 
+            // dgEmail
+            // 
+            this.dgEmail.DataPropertyName = "Email";
+            this.dgEmail.HeaderText = "Email";
+            this.dgEmail.MinimumWidth = 8;
+            this.dgEmail.Name = "dgEmail";
+            this.dgEmail.ReadOnly = true;
+            this.dgEmail.Width = 150;
+            // 
+            // dgPhone
+            // 
+            this.dgPhone.DataPropertyName = "Phone";
+            this.dgPhone.HeaderText = "Phone";
+            this.dgPhone.MinimumWidth = 8;
+            this.dgPhone.Name = "dgPhone";
+            this.dgPhone.ReadOnly = true;
+            this.dgPhone.Width = 150;
+            // 
+            // dgEmpId
+            // 
+            this.dgEmpId.DataPropertyName = "EmpId";
+            this.dgEmpId.HeaderText = "EmpId";
+            this.dgEmpId.MinimumWidth = 8;
+            this.dgEmpId.Name = "dgEmpId";
+            this.dgEmpId.ReadOnly = true;
+            this.dgEmpId.Width = 150;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 180);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "EmpId:";
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(178, 80);
+            this.txt_Email.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(318, 30);
+            this.txt_Email.TabIndex = 6;
+            this.txt_Email.Leave += new System.EventHandler(this.txt_Email_Leave);
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(178, 123);
+            this.txt_phone.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(318, 30);
+            this.txt_phone.TabIndex = 7;
+            this.txt_phone.Leave += new System.EventHandler(this.txt_phone_Leave);
+            // 
+            // txt_EmpId
+            // 
+            this.txt_EmpId.Location = new System.Drawing.Point(178, 173);
+            this.txt_EmpId.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_EmpId.Name = "txt_EmpId";
+            this.txt_EmpId.Size = new System.Drawing.Size(318, 30);
+            this.txt_EmpId.TabIndex = 8;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(108, 287);
+            this.btn_save.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(100, 42);
+            this.btn_save.TabIndex = 9;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(240, 285);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(100, 44);
+            this.btn_delete.TabIndex = 10;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(406, 287);
+            this.btn_cancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(100, 42);
+            this.btn_cancel.TabIndex = 11;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(1250, 558);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.txt_EmpId);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvEmployee);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.DataGridView dgvEmployee;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.TextBox txt_EmpId;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgEmail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgPhone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgEmpId;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        info model = new info();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+        string phonepattern = "^[ ()+]*([0-9][ ()+]*){10}$";
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        void clear()
+        {
+            txt_Name.Text = txt_Email.Text = txt_phone.Text = txt_EmpId.Text = "";
+            btn_save.Text = "save";
+            btn_delete.Enabled=false;
+            model.Id = 0;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clear();
+            this.ActiveControl = txt_Name;
+            LoadData();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+           
+            model.Name = txt_Name.Text;
+            model.Email = txt_Email.Text;
+            model.Phone = txt_phone.Text;
+            model.EmpId =Convert.ToInt32( txt_EmpId.Text);
+
+            using (PersonalInfoEntities1 db = new PersonalInfoEntities1())
+            {
+                if (model.Id == 0)
+                {
+                    db.infoes.Add(model);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.Entry(model).State=EntityState.Modified;
+                    db.SaveChanges();
+                }
+                
+
+            }
+            if ((string.IsNullOrEmpty(txt_Name.Text)) && (((TextBox)sender).Text.Length < 7))
+            {
+                txt_Name.Focus();
+                errorProvider1.SetError(this.txt_Name, "PLease provide atleast 7 characters");
+
+            }
+
+            else if (string.IsNullOrEmpty(txt_Email.Text))
+            {
+                txt_Email.Focus();
+                errorProvider2.SetError(this.txt_Email, "!!!Invalid Email!!!");
+            }
+
+            else if(string.IsNullOrEmpty(txt_phone.Text))
+            {
+                txt_phone.Focus();
+                errorProvider3.SetError(this.txt_phone, "Please provide 10 digit phone number");
+            }
+            else
+            {
+                clear();
+                LoadData();
+                MessageBox.Show("Submitted successfully");
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Do you want to delete this record","Message",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                using (PersonalInfoEntities1 db = new PersonalInfoEntities1())
+                {
+                    db.infoes.Attach(model);
+                    db.infoes.Remove(model);
+                    db.SaveChanges();
+                    LoadData();
+                    clear();
+                    MessageBox.Show("Deleted succesfully");
+                }
+            }
+        }
+
+        void LoadData()
+        {
+            using (PersonalInfoEntities1 db = new PersonalInfoEntities1())
+            {
+                dgvEmployee.DataSource = db.infoes.ToList < info>();
+            }
+        }
+
+        private void dgvEmployee_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvEmployee.CurrentRow.Index != -1)
+            {
+                model.Id = Convert.ToInt32(dgvEmployee.CurrentRow.Cells["dgId"].Value);
+                using (PersonalInfoEntities1 db = new PersonalInfoEntities1())
+                {
+                    model = db.infoes.Where(x => x.Id == model.Id).FirstOrDefault();
+                    txt_Name.Text = model.Name;
+                    txt_Email.Text = model.Email;
+                    txt_phone.Text = model.Phone;
+                    txt_EmpId.Text = Convert.ToString(model.EmpId);
+                }
+
+                btn_save.Text = "Update";
+                btn_delete.Enabled = true;
+            }
+        }
+
+        //private void txt_Name_TextChanged(object sender, EventArgs e)
+        //{
+        //    //if (((TextBox)sender).Text.Length!= 5)
+        //    //{
+        //    //    MessageBox.Show("You need to write at least 5 characters");
+        //    //}
+
+        //    if ((string.IsNullOrEmpty(txt_Name.Text)==false) && (((TextBox)sender).Text.Length<7))
+        //    {
+        //        txt_Name.Focus();
+        //        errorProvider1.SetError(this.txt_Name, "PLease provide atleast 7 characters");
+                
+        //    }
+        //    else
+        //    {
+        //        errorProvider1.Clear();
+        //    }
+        //}
+
+        //private void txt_Email_TextChanged(object sender, EventArgs e)
+        //{
+           
+            
+           
+
+        //        if (Regex.IsMatch(txt_Email.Text, pattern) == false)
+        //        {
+        //            txt_Email.Focus();
+        //            errorProvider2.SetError(this.txt_Email, "!!!Invalid Email!!!");
+
+                    
+        //        }
+        //        else
+        //        {
+        //            errorProvider2.Clear();
+        //        }
+            
+        //}
+
+        //private void txt_phone_TextChanged(object sender, EventArgs e)
+        //{
+            
+           
+        //    if(Regex.IsMatch(txt_phone.Text, phonepattern) ==false)
+        //    {
+                
+        //        txt_phone.Focus();
+        //        errorProvider3.SetError(this.txt_phone, "Please provide 10 digit phone number");
+        //    }
+        //    else
+        //    {
+        //        errorProvider3.Clear();
+                
+        //    }
+        //}
+
+        private void txt_Name_Leave(object sender, EventArgs e)
+        {
+            if ((string.IsNullOrEmpty(txt_Name.Text) == false) && (((TextBox)sender).Text.Length < 7))
+            {
+                txt_Name.Focus();
+                errorProvider1.SetError(this.txt_Name, "PLease provide atleast 7 characters");
+
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void txt_Email_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(txt_Email.Text, pattern) == false)
+            {
+                txt_Email.Focus();
+                errorProvider2.SetError(this.txt_Email, "!!!Invalid Email!!!");
+
+
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+        }
+
+        private void txt_phone_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(txt_phone.Text, phonepattern) == false)
+            {
+
+                txt_phone.Focus();
+                errorProvider3.SetError(this.txt_phone, "Please provide 10 digit phone number");
+            }
+            else
+            {
+                errorProvider3.Clear();
+
+            }
+        }
+
+        
+    }
+}
